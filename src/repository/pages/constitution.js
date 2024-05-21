7ff4cbd8502dc659f24add9f2a80c179367a9291
Sonnet Songbird <sonnet4235@gmail.com>
2024-05-21 14:56:24 +0900

    CHORE: Add html based mockContent constitution.js

---
+++ src/repository/pages/constitution.js
@@ -0,0 +1,139 @@
+const constitution = {
+    "pages": [
+        {
+            "idx": 1,
+            "content": "<h1 style=\"text-align: center;\">회 칙</h1>\n" +
+                "    <p style=\"text-align: right; margin-block: 0.3rem\">1980년 11월 9일 개정</p>\n" +
+                "    <p style=\"text-align: right; margin-block: 0.3rem;\">1995년 11월 9일 개정</p>\n" +
+                "    <p style=\"text-align: right; margin-block: 0.3rem;\">1997년 11월 15일 개정</p>\n" +
+                "    <p style=\"text-align: right; margin-block: 0.3rem;\">2003년 11월 14일 개정</p>\n" +
+                "    <p style=\"text-align: right; margin-block: 0.3rem;\">2004년 2월 13일 개정</p>\n" +
+                "    <p style=\"text-align: right; margin-block: 0.3rem;\">2006년 2월 7일 개정</p>\n" +
+                "    <p style=\"text-align: right; margin-block: 0.3rem;\">2010년 6월 29일 개정</p>\n" +
+                "    <p style=\"text-align: right; margin-block: 0.3rem;\">2014년 5월 11일 개정</p>\n" +
+                "    <h2 style=\"text-align: center;\">제 1장&nbsp;&nbsp;총&nbsp;&nbsp;&nbsp;&nbsp;칙</h2>\n" +
+                "    <h4 style=\"margin-block: 1rem;\">제 1 조 (명 칭) <span style=\"font-weight: normal;\">본회는 재경 해동고등학교총동창회라 칭한다</span></h4>\n" +
+                "    <h4 style=\"margin-block: 1rem;\">제 2 조 (목 적) <span style=\"font-weight: normal;\">본회는 모교 및 본부동문회와의 유대를 강화하고 회원 상호간의 친목을 도모하며 상부상조의 정신을 구현함을 목적으로 한다.</span>\n" +
+                "    </h4>\n" +
+                "    <h4 style=\"margin-block: 1rem;\">제 3 조 (회 원) <span style=\"font-weight: normal;\">본회의 회원 자격은 해동고등학교 졸업자 및 1년이상 재학한 자로서 서울시와 수도권에 거주하거나 직장 등에 연고가 있는 동창으로 한다.</span>\n" +
+                "    </h4>\n" +
+                "    <h4 style=\"margin-block: 1rem;\">제 4 조 (사무소) <span style=\"font-weight: normal;\">본회의 사무소는 서울특별시에 둔다.</span></h4>"
+        }
+        , {
+            "idx": 2,
+            "content": "<h2 style=\"text-align: center;\">제 2장&nbsp;&nbsp;조&nbsp;&nbsp;&nbsp;&nbsp;직</h2>\n" +
+                "    <h4>제 5 조 (임 원) <span style=\"font-weight: normal;\">본회는 다음의 임원을 둔다.</span></h4>\n" +
+                "    <ol type=\"1\">\n" +
+                "        <li>회&nbsp;&nbsp;&nbsp;장 : 1 명</li>\n" +
+                "        <li>\n" +
+                "            <ul style=\"list-style: none; padding-left: 0\">\n" +
+                "                <li>부회장 : 수석부회장 1 명 (회장기수+1 기수회장)</li>\n" +
+                "                <li style=\" padding-left: 4rem \">CM부회장 1 명</li>\n" +
+                "                <li style=\" padding-left: 4rem \">부회장 5 명 (회장기수+2,3,4,5,6 기수회장)</li>\n" +
+                "            </ul>\n" +
+                "        <li>이&nbsp;&nbsp;&nbsp;사 : 년회비 납부대상 기수회장 29명 <br>\n" +
+                "            <span style=\"padding-left: 4rem\">(36세~70세, 부회장 6명 중복제외)</span>\n" +
+                "        </li>\n" +
+                "        <li>감&nbsp;&nbsp;&nbsp;사 : 2 명 이내</li>\n" +
+                "        <li>사무국장 1 명 및 부장 5명 이내</li>\n" +
+                "    </ol>\n" +
+                "    <h4>제 6 조 (명예회장등) <span style=\"font-weight: normal;\">본회는 다음 각호에 의한 명예회장과 특별위원을 둔다.</span></h4>\n" +
+                "    <ol type=\"1\">\n" +
+                "        <li>명예회장은 역대회장으로 추대한다.</li>\n" +
+                "        <li>특별위원은 본회에 일정액 이상의 찬조금을 내거나 본회의 발전에 기여한 회원 중에서 추대한다.</li>\n" +
+                "    </ol>\n" +
+                "    <h4>제 7 조 (임원의 임기) <span style=\"font-weight: normal;\">임원의 임기는 2년으로 하고 연임할 수 있다. 단 보궐선거에 의하여 선출된 임원의 임기는 전임자의 잔여기간으로 한다. (회장의 임기는 1월1일에서 익년 12월31일까지 2년으로 한다)</span>\n" +
+                "    </h4>\n" +
+                "    <h4>제 8 조 (임원의 선출) <span style=\"font-weight: normal;\">임원은 총회에서 선출한다.</span></h4>\n" +
+                "    <h4>제 9 조 (임원의 직무)</h4>\n" +
+                "    <ol type=\"1\">\n" +
+                "        <li>회장은 본회를 대표하여 본회를 총괄하며 각종 회의의 의장이 된다.</li>\n" +
+                "        <li>부회장은 회장을 보좌하며 회장 유고시에는 수석부회장이 그 직무를 대행한다. 수석부회장은 결격사유가 없는 한 차기회장직을 승계할 수 있다.</li>\n" +
+                "        <li>감사는 본회 업무와 회계 및 이에 관련된 업무를 감사하며 총회에서 보고한다.</li>\n" +
+                "    </ol>\n" +
+                "    <h4>제 10조 (부서 및 산하조직)</h4>\n" +
+                "    <ol type=\"1\">\n" +
+                "        <li>본회의 조직강화와 원활한 운영을 위하여 사무국장과 총무, 체육, 재정 및 홍보담당 등을 둘 수 있다.</li>\n" +
+                "        <li>본회 내에 직능별 동창회와 취미 서클 등을 조직 운영할 수 있다.</li>\n" +
+                "    </ol>"
+        }
+        , {
+            "idx": 3,
+            "content": "<h2 style=\"text-align: center;\">제 3장&nbsp;&nbsp;회&nbsp;&nbsp;&nbsp;&nbsp;의</h2>\n" +
+                "    <h4>제 11 조 (총 회)</h4>\n" +
+                "    <ol type=\"1\">\n" +
+                "        <li>총회는 임원 선출, 회칙개정 및 예산, 결산과 기타 이사회에서 부의된 사항을 심의 의결한다.</li>\n" +
+                "        <li>총회는 정기총회와 임시총회로 구분한다. 정기총회는 회계연도 마감일부터 1개월 이내에 회장이 이를 소집한다. 임시총회는 회장이 필요하다고 인정하거나 이사회의 의결 또는 회원 1/3이상의 서면요구가\n" +
+                "            있을 때 회장이 이를 소집한다.\n" +
+                "        </li>\n" +
+                "        <li>총회의 의안은 참석회원 과반수의 찬성으로 의결하며 가부동수인 경우에는 의장이 결정한다.</li>\n" +
+                "    </ol>\n" +
+                "    <h4>제 12 조 (이사회 구성 및 임무)</h4>\n" +
+                "    <ol type=\"1\">\n" +
+                "        <li>이사회는 회장, 부회장과 이사로써 구성한다. 고문,자문위원 및 감사는 이사회에 출석하여 의견을 개진할 수 있다.</li>\n" +
+                "        <li>이사회는 본회 운영을 위하여 다음사항을 관장한다.</li>\n" +
+                "    </ol>\n" +
+                "    <ol type=\"1\">\n" +
+                "        <li>예산안 및 결산 승인</li>\n" +
+                "        <li>총회의 위임사항</li>\n" +
+                "        <li>총회에 제출한 의안</li>\n" +
+                "        <li>기타 본회 운영에 관한 중요사항</li>\n" +
+                "    </ol>\n" +
+                "    <h4>제 13 조 (이사회 소집)</h4>\n" +
+                "    <ol type=\"1\">\n" +
+                "        <li>이사회는 회장이 필요하다고 인정할 때 이를 소집한다.</li>\n" +
+                "        <li>회장은 재적이사 과반수의 요청이 있을 때에는 이사회를 소집해야 한다.</li>\n" +
+                "    </ol>\n" +
+                "    <h4>제 14 조 (이사회 등의 의결)</h4>\n" +
+                "    <ol type=\"1\">\n" +
+                "        <li>이사회는 이사 출석으로 성립하고 출석인원 과반수의 찬성으로 의결한다.</li>\n" +
+                "        <li>총회는 출석회원으로 개최하고 출석회원 과반수로 의결한다. 단, 우편 또는 FAX로 위임장을 제출한 경우에도 출석으로 간주한다.</li>\n" +
+                "    </ol>\n" +
+                "    <h4>제 15 조 (회장단 회의)</h4>\n" +
+                "    <ol type=\"1\">\n" +
+                "        <li>본회의 효율적인 운영을 위하여 회장단 회의를 둔다.</li>\n" +
+                "        <li>회장단 회의는 고문, 자문위원, 회장, 부회장, 감사 및 사무국장으로 구성한다. 다만 고문, 자문위원, 감사 및 사무국장은 의견만을 제시할 수 있다.</li>\n" +
+                "        <li>회장단 회의는 다음 사항을 심의 결정한다.</li>\n" +
+                "    </ol>\n" +
+                "    <ol style=\"list-style-type:none\">\n" +
+                "        <li>가. 이사회의 위임사항</li>\n" +
+                "        <li>나. 총회 및 이사회에 상정할 의안의 예비심사에 관한 사항</li>\n" +
+                "        <li>다. 예산의 전용 및 예비비 지출에 관한 사항</li>\n" +
+                "        <li>라. 회원의 상벌 심사 및 징계에 관한 사항 (공로자에게는 포상, 동창회 명예훼손자에게는 징계처리)</li>\n" +
+                "        <li>마. 기타 본회 운영에 관하여 긴급을 요하는 사항</li>\n" +
+                "    </ol>"
+        }
+        , {
+            "idx": 4,
+            "content": "<h2 style=\"text-align: center;\">제 4장&nbsp;&nbsp;재&nbsp;&nbsp;&nbsp;&nbsp;정</h2>\n" +
+                "    <h4>제 16 조 (회계년도) <span style=\"font-weight: normal;\">본회의 회계연도는 매년 12월1일부터 익년도 11월30일까지로 한다.</span></h4>\n" +
+                "    <h4>제 17 조 (재 정) <span style=\"font-weight: normal;\">본회의 재정은 다음과 같은 수입으로 충당한다.</span></h4>\n" +
+                "    <ol type=\"1\">\n" +
+                "        <li>기수별 년회비\n" +
+                "            <ul style=\"list-style-type: none\">\n" +
+                "                <li>61세~70세 해당 기수 (10개 기수) : 각 50만원</li>\n" +
+                "                <li>41세~60세 해당 기수 (20개 기수) : 각 100만원</li>\n" +
+                "                <li>36세~40세 해당 기수 (5개 기수) : 각 50만원</li>\n" +
+                "            </ul>\n" +
+                "        </li>\n" +
+                "\n" +
+                "        <li>특별 찬조금</li>\n" +
+                "        <li>기타 수입금</li>\n" +
+                "    </ol>\n" +
+                "    <h4>제 18 조 (회계보고) <span style=\"font-weight: normal;\">사무국장은 해당 년도 회계의 수지를 결산하여 감사의 감사를 거쳐 이사회에 보고한 다음 총회의 승인을 받아야 한다.</span></h4>\n" +
+                "    <h4>제 19 조 (경,조사지원) <span style=\"font-weight: normal;\">상기 제 17조(재정) 제 2,3항에 의거 임원분담금 및 특별찬조금을 납부한 임원 및 회원에 한해서 년 1회 경조사 발생시 화환(금)을 지원한다.</span></h4>\n"
+        }, {
+            "idx": 5,
+            "content": "<h2 style=\"text-align: center;\">부&nbsp;&nbsp;&nbsp;&nbsp;칙</h2>\n" +
+                "    <ol type=\"1\">\n" +
+                "        <li>본회의 시행을 위하여 필요한 사항은 이사회에서 정한다.</li>\n" +
+                "        <li>본 회칙에 규정되어 있지 아니한 것은 해동고등학교 동창회 본부의 회칙 또는 일반관례에 의한다.</li>\n" +
+                "        <li>본 개정회칙은 2014년 5월 11일부터 시행한다.</li>\n" +
+                "    </ol>"
+        }
+    ]
+    , "defaultTheme": "앨범 템플릿"
+}
+
+
+export default constitution
