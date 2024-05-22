96c5e740271cf80102b2d30833f9059714799eee
Sonnet Songbird <sonnet4235@gmail.com>
2024-05-22 14:17:23 +0900

    FEAT: Implement ProfileModel fetcher

--- src/model/ProfileModel.js
+++ src/model/ProfileModel.js
@@ -1,7 +1,33 @@
 //@Component: profile, contact(list)
+export class DirectoryModel {
+    constructor() {
+        this.profile = this.fetchProfiles()
+    }
+
+    fetchProfiles() {
+        let directory = []
+        try {
+            const directoryRepo = directory.profiles
+            directoryRepo.forEach((item) => {
+                const i = item.profile
+                directory.push(new ProfileModel(
+                    i.belong
+                    , i.name
+                    , i.contact
+                    , i.keyword
+                    , i.note
+                ))
+            })
+
+        } catch (error) {
+            console.error('Failed to fetch profile data:', error);
+        }
+        return directory;
+    }
+}
 
 export class ProfileModel {
-    //@arg: contact: [string]
+    //@arg: contact: [string], note: [string], keyword[string]
     constructor(belong, name, contact, note, keyword) {
         this.belong = belong ? belong : "";
         this.name = name ? name : "";
@@ -10,3 +36,4 @@ export class ProfileModel {
         this.keyword = keyword ? keyword : [""];
     }
 }
+
