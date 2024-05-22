210bb8b1cfff8bdeba0d18423eb57a48d0a54d00
Sonnet Songbird <sonnet4235@gmail.com>
2024-05-22 14:18:18 +0900

    CHORE: Add profile directory mock repo directory.js

---
+++ src/repository/Profile/directory.js
@@ -0,0 +1,36 @@
+const directory = {
+    "profiles": [
+        {
+            "idx": 1
+            , "profile": {
+                "belong": "해동고 41기"
+                , "name": "김재희"
+                , "contact": ["010-2667-5722", "02-711-5722", "toj@t2l.co.kr"]
+                , "keyword": ["7반"]
+                , "note": ["(주)티투엘 대표"]
+            }
+        },
+        {
+            "idx": 2
+            , "profile": {
+                "belong": "해동고 41기"
+                , "name": "권도철"
+                , "contact": ["010-1234-5678", "id@domain.com"]
+                , "keyword": ["7반"]
+                , "note": []
+            }
+        },
+        {
+            "idx": 3
+            , "profile": {
+                "belong": "티투엘"
+                , "name": "홍길동"
+                , "contact": ["010-1234-5678", "id@t2l.co.kr"]
+                , "keyword": []
+                , "note": ["테스트 테스트"]
+            }
+        }
+    ]
+}
+
+export default directory
