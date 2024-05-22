4de02019c29b43174f5b2798bf2e440d8c96965b
Sonnet Songbird <sonnet4235@gmail.com>
2024-05-22 14:49:55 +0900

    FEAT: Make Component Profile to Modal

--- src/component/Profile.js
+++ src/component/Profile.js
@@ -1,38 +1,50 @@
 //TODO:Not tested
-export function Profile({model}) {
+import {useEffect, useState} from "react";
+import {Modal} from "react-bootstrap";
+
+export function Profile({model, functions}) {
+    const [show, setShow] = useState(false);
+    const modalClose = () => setShow(false);
+    const modalShow = () => setShow(true);
+
+    useEffect(() => {
+        functions["modalShow"] = setShow
+    }, [functions]);
 
     return (
-        <div className="profile-container">
-            <table>
-                <thead>
-                    <tr>
-                        <th>Belong</th>
-                        <th>Name</th>
-                        <th>Contact</th>
-                        <th>Note</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>{model.belong}</td>
-                        <td>{model.name}</td>
-                        <td>
-                            <ul>
-                                {model.contact.map((item, index) => (
-                                    <li key={index}>{item}</li>
-                                ))}
-                            </ul>
-                        </td>
-                        <td>
-                            <ul>
-                                {model.note.map((item, index) => (
-                                    <li key={index}>{item}</li>
-                                ))}
-                            </ul>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
+        <Modal show={show} onHide={modalClose}>
+            <div className="profile-container">
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Belong</th>
+                            <th>Name</th>
+                            <th>Contact</th>
+                            <th>Note</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>{model.belong}</td>
+                            <td>{model.name}</td>
+                            <td>
+                                <ul>
+                                    {model.contact.map((item, index) => (
+                                        <li key={index}>{item}</li>
+                                    ))}
+                                </ul>
+                            </td>
+                            <td>
+                                <ul>
+                                    {model.note.map((item, index) => (
+                                        <li key={index}>{item}</li>
+                                    ))}
+                                </ul>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </Modal>
     );
 }
