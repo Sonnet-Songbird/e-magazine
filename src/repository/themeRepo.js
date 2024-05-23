647e28baed89da792fa8675edfa8648bdeea4b23
Sonnet Songbird <sonnet4235@gmail.com>
2024-05-23 14:40:47 +0900

    FEAT: Make Infinity pagerPerView in DemoDesktopViewTheme Temporarily.

--- src/repository/themeRepo.js
+++ src/repository/themeRepo.js
@@ -8,7 +8,7 @@ const themeRepo = () => {
         repository[name] = {idx: Object.keys(repository).length, component: component, pagePerView: pagePerView};
     }
 
-    add("데모 템플릿", DemoDesktopViewTemplate, 2);
+    add("데모 템플릿", DemoDesktopViewTemplate, Infinity);
     add("앨범 템플릿", PaperAlbumDesktopTheme, Infinity); //가변 개수 템플릿으로 만들 예정
     return repository;
 };
--- src/template/view/DemoDesktopViewTheme.js
+++ src/template/view/DemoDesktopViewTheme.js
@@ -6,12 +6,7 @@ const DemoDesktopViewTheme = ({contents}) => {
             <div>
                 데모 뷰어
             </div>
-            <div className="content1">
-                {contents[0] !== undefined && contents[0]}
-            </div>
-            <div className="content2">
-                {contents[1] !== undefined && contents[1]}
-            </div>
+            {contents}
         </div>
     )
 }
