8fa5f9de15c07ad72c1db77a0dd0ef2e5a96e987
Sonnet Songbird <sonnet4235@gmail.com>
2024-05-29 16:54:40 +0900

    FEAT: Add component MobileOffcanvas.js

---
+++ src/component/fragment/MobileOffcanvas.js
@@ -0,0 +1,26 @@
+import {useState} from 'react';
+import Button from 'react-bootstrap/Button';
+import Offcanvas from 'react-bootstrap/Offcanvas';
+
+export default function OffCanvasExample({name, children, ...props}) {
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const toggleShow = () => setShow((s) => !s);
+
+    return (
+        <>
+            <Button variant="primary" onClick={toggleShow} className="me-2, offcanvas-button">
+                {name}
+            </Button>
+            <Offcanvas show={show} onHide={handleClose} {...props}>
+                <Offcanvas.Header closeButton>
+                    <Offcanvas.Title>메뉴</Offcanvas.Title>
+                </Offcanvas.Header>
+                <Offcanvas.Body>
+                    {children}
+                </Offcanvas.Body>
+            </Offcanvas>
+        </>
+    );
+}
