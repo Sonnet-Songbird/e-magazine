64bf8582e25f77a993379f7657746b8f9e261974
Sonnet Songbird <sonnet4235@gmail.com>
2024-05-29 16:54:31 +0900

    FEAT: Add component CarouselComp.js

---
+++ src/component/fragment/CarouselComp.js
@@ -0,0 +1,24 @@
+import Carousel from 'react-bootstrap/Carousel';
+import {Children} from "react";
+
+// @arg items: [component]
+function CarouselComp({children, active, goToFunc, interval = null}) {
+
+    const renderItems = () => {
+        return Children.map(children, (child, idx) => (
+            <Carousel.Item key={idx}>
+                {child}
+                {/*<Carousel.Caption>*/}
+                {/*</Carousel.Caption>*/}
+            </Carousel.Item>
+        ));
+    };
+
+    return (
+        <Carousel activeIndex={active} onSelect={goToFunc} interval={interval} indicators={false}>
+            {renderItems()}
+        </Carousel>
+    );
+}
+
+export default CarouselComp;
